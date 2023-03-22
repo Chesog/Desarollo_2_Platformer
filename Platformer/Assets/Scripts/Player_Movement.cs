a2a0ad2401e37c9dbb9c194cfd9c309e19804c41
Dario <otrantodario@gmail.com>
2023-03-22 10:01:37 -0300

    Add Player Simple Movement

--- Platformer/Assets/Scripts/Player_Movement.cs
+++ Platformer/Assets/Scripts/Player_Movement.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector2 _horDirection;
+    [SerializeField] private float Speed;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
+
     void Update()
     {
-        
+        _horDirection = ReadHorDirection();
+        transform.position += new Vector3(_horDirection.x,0,_horDirection.y) * Speed * Time.deltaTime;
+    }
+
+    private Vector2 ReadHorDirection() 
+    {
+        var result = new Vector2
+        {
+            x = Input.GetAxis("Horizontal"),
+            y = Input.GetAxis("Vertical")
+        };
+
+        return result;
     }
 }
