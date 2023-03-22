8797a2e2216abb497e4c4a5b895f6ede93f69af9
Dario <otrantodario@gmail.com>
2023-03-22 10:45:26 -0300

    Add Player Jumping

--- Platformer/Assets/Scripts/Player_Movement.cs
+++ Platformer/Assets/Scripts/Player_Movement.cs
@@ -5,23 +5,65 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    private Vector2 _horDirection;
-    [SerializeField] private float Speed;
+    private Vector2 horDirection;
+    private bool isJumpinginput;
+    [Range(0, 500)][SerializeField] private float speed = 10;
+    [Range(0, 100)][SerializeField] private float jumpForce = 10;
+    Rigidbody rigidbody;
 
-    void Start()
+
+    private void Awake()
     {
-        
+            rigidbody ??= GetComponent<Rigidbody>();
+        if (!rigidbody)
+        {
+            Debug.LogError(message: $"{name}: (logError){nameof(rigidbody)} is null");
+        }
     }
-
-   
 
     void Update()
     {
-        _horDirection = ReadHorDirection();
-        transform.position += new Vector3(_horDirection.x,0,_horDirection.y) * Speed * Time.deltaTime;
+        horDirection = ReadHorDirection();
+        isJumpinginput = ReadJumpInput();
     }
 
-    private Vector2 ReadHorDirection() 
+    private void FixedUpdate()
+    {
+        if (rigidbody)
+        {
+            rigidbody.velocity = GetVelocity();
+        }
+
+        if (isJumpinginput && rigidbody) 
+        {
+            rigidbody.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
+            isJumpinginput = false;
+        }
+    }
+
+    /// <summary>
+    /// Return the value for the jump input
+    /// </summary>
+    /// <returns>True if the button for jump is presed</returns>
+    private bool ReadJumpInput() 
+    {
+        return Input.GetButtonUp("Jump");
+    }
+
+    /// <summary>
+    /// Calculate The RigidBody Velocity
+    /// </summary>
+    /// <returns>A Vector3</returns>
+    private Vector3 GetVelocity()
+    {
+        return new Vector3(horDirection.x, 0, horDirection.y) * speed + Vector3.up * rigidbody.velocity.y;
+    }
+
+    /// <summary>
+    /// Return the value for the Horizontal && vertical axis inputs
+    /// </summary>
+    /// <returns> A Vector 2</returns>
+    private Vector2 ReadHorDirection()
     {
         var result = new Vector2
         {
