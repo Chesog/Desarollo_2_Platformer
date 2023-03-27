331be8d362844eeb9073173d61057eafa174a478
Dario <otrantodario@gmail.com>
2023-03-27 12:00:07 -0300

    Fix Player Infinite Jump

--- Platformer/Assets/Scripts/Characters/Character_Movements.cs
+++ Platformer/Assets/Scripts/Characters/Character_Movements.cs
@@ -6,19 +6,51 @@ using UnityEngine;
 
 public class Character_Movements : MonoBehaviour
 {
+
+    [Header("SetUp")]
+    [SerializeField] Rigidbody rigidbody;
+    [SerializeField] GameObject feet_Pivot;
+
+    [Header("Movement")]
+    [SerializeField] Vector3 _CurrentMovement;
     [Range (0,100)] [SerializeField] float speed = 20.0f;
     [Range (0,100)] [SerializeField] float jumpForce = 20.0f;
-    [SerializeField] Vector3 _CurrentMovement;
-    [SerializeField] Rigidbody rigidbody;
+    [SerializeField] bool canJunp;
 
-    private void Start()
+    [Header("SetUp")]
+    [SerializeField] const float maxDistance = 0f;
+    [SerializeField] const float minJumpDistance = 0.5f;
+
+    private void Awake()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        rigidbody ??= GetComponent<Rigidbody>();
+        if (!rigidbody)
+        {
+            Debug.LogError(message: $"{name}: (logError){nameof(rigidbody)} is null");
+        }
+
+        feet_Pivot ??= GetComponent<GameObject>();
+        if (!feet_Pivot)
+        {
+            Debug.LogError(message: $"{name}: (logError){nameof(feet_Pivot)} is null");
+        }
+
+        canJunp = true;
     }
 
     private void Update()
     {
         transform.Translate(speed * Time.deltaTime * _CurrentMovement);
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (canJunp && Physics.Raycast(feet_Pivot.transform.position,Vector3.down,out hit,maxDistance) && hit.distance <= minJumpDistance) 
+        {
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            canJunp = false;
+        }
     }
 
     public void OnMove(InputValue input)
@@ -30,7 +62,11 @@ public class Character_Movements : MonoBehaviour
 
     public void OnJump()
     {
-        Debug.Log("Jump");
-        rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        canJunp = true;   
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
     }
 }
